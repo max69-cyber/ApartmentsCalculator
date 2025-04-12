6516c7415a713676350076600d0bb2abd94bd297
RayXaus <137321629+max69-cyber@users.noreply.github.com>
2025-04-12 08:59:08 +0500

    Добавьте файлы проекта.

---
+++ ApartmentsCalculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApartmentsCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 30;
+            int M = 10;
+            int X = 10000;
+
+            int result = CalculateRevenue(N, M, X);
+            Console.WriteLine($"Общая выручка: {result}");
+            Console.ReadKey();
+        }
+
+        public static int CalculateRevenue(int N, int M, int X)
+        {
+            int totalRevenue = 0;
+            int currentPrice = X;
+
+            for (int i = 1; i <= N; i++)
+            {
+                totalRevenue += currentPrice;
+
+                if (i % M == 0)
+                {
+                    currentPrice += 1000;
+                }
+            }
+
+            return totalRevenue;
+        }
+
+
+    }
+}
+
